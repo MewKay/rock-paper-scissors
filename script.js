aa0d7e1e5dda5bc8d1bfb79cc9839264a222cddb
MewKay <125735249+MewKay@users.noreply.github.com>
2023-05-16 00:19:05 +0300

    Write 'checkIfPlayerIsWinning' function

--- script.js
+++ script.js
@@ -15,13 +15,20 @@ function playRound (playerSelection, computerSelection) {
     return "It's a draw !";
   }
   
-  if(checkIfPlayerIsWinning()) {
+  if(checkIfPlayerIsWinning(playerSelection, computerSelection)) {
     return `You Won! ${playerSelection} beats ${computerSelection}`;
   }
   
   else return `You Lose! ${computerSelection} beats ${playerSelection}`;
 }
 
+function checkIfPlayerIsWinning(playerSelection, computerSelection) {
+  if( (playerSelection === "Rock" && computerSelection === "Scissors") ||
+      (playerSelection === "Paper" && computerSelection === "Rock") ||
+      (playerSelection === "Scissors" && computerSelection === "Paper"))
+      return true;
+  else return false;
+}
 
 function getRandomBetween(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
